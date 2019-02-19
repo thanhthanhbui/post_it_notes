a2a32a8340e2b7e08cd650c6ee6a0c20c218ca51
Thanh Bui <ttthanhbui@gmail.com>
2019-02-18 22:32:34 -0700

    Post It Notes

---
+++ db/migrate/20190218222242_create_notes.rb
@@ -0,0 +1,11 @@
+class CreateNotes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :notes do |t|
+      t.string :color
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
